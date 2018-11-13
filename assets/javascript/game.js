9c44a6a768d59e3cffae7f84101589b1ca521e45
sayex <elsayer@msn.com>
2018-11-13 12:17:43 -0700

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
 
     }
 
-    // screen updates are
+    // screen updates
 
     function screenUpdate() {
         $(".numberToGuess").text(randomNumbersObject.NumberToGuess);
